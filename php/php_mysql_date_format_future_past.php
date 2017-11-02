303aeec8fea5c1ec42e0e324bfbbebf0f6c5ecfd
maranemil <databas3>
2017-11-02 22:27:32 +0100

    howto

--- php/php_mysql_date_format_future_past.php
+++ php/php_mysql_date_format_future_past.php
@@ -231,3 +231,23 @@ sudo service mysql restart to restart MySQL
 SELECT @@sql_mode
 ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION
 SET sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+
+
+###################################################
+#
+# #1451 - Cannot delete or update a parent row: a foreign key constraint fails
+#
+###################################################
+
+
+SET FOREIGN_KEY_CHECKS=0; -- to disable them
+# Do here update or delete
+SET FOREIGN_KEY_CHECKS=1; -- to re-enable them
+
+###################################################
+#
+# Replace regex oracle
+# https://docs.oracle.com/cd/B19306_01/server.102/b14200/functions130.htm
+###################################################
+
+
