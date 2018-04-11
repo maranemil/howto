899a0f4c9c3af6bdc53cf5dcda578b127a2569da
maranemil <maran_emil@yahoo.com>
2018-04-11 21:41:38 +0200

    Merge branch 'master' of https://github.com/maranemil/howto
    # Conflicts:
    #	datamining/datasets/weka/autompg.arff.txt

---
+++ datamining/opencv_machinelearning/machine_learning_recipes_youtube.py
@@ -0,0 +1,16 @@
+
+"""
+###############################################
+Hello World - Machine Learning Recipes #1
+https://www.youtube.com/watch?v=cKxRvEZd3Mw&list=PLOU2XLYxmsIIuiBfYad6rFYQU_jL2ryal
+// write py file
+###############################################
+"""
+
+from sklearn import tree
+features [[140,1], [130,1], [150,0],[170,0]]
+labels[0,0,1,1]
+clf = tree.DecisionTreeClassifier()
+clf = clf-fit(features, labels)
+print clf.predict([[150,0]])
+
