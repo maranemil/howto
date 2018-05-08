d4154f1fd35ea8a6367099a72c0073757991b7f8
Maran Emil <maran.emil@gmail.com>
2018-05-08 17:06:56 +0200

    howto

--- fullstack/php/php_howto.php
+++ fullstack/php/php_howto.php
@@ -995,6 +995,27 @@ namespace bar;
 
 
 
+####################################################################
+
+php base64 decode image
+
+####################################################################
+
+$data = 'iVBORw0KGgoAAAANSUhEUgAAABwAAAASCAMAAAB/2U7WAAAABlBMVEUAAAD///+l2Z/dAAAASUlEQVR4XqWQUQoAIAxC2/0vXZDrEX4IJTRkb7lobNUStXsB0jIXIAMSsQnWlsV+wULF4Avk9fLq2r8a5HSE35Q3eO2XP1A1wQkZSgETvDtKdQAAAABJRU5ErkJggg==';
+$data = base64_decode($data);
+
+$im = imagecreatefromstring($data);
+if ($im !== false) {
+    header('Content-Type: image/png');
+    imagepng($im);
+    imagedestroy($im);
+}
+else {
+    echo 'An error occurred.';
+}
+
+
+
 
 
 
