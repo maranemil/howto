86bbc838440dc12f9660aa175e95d9eaa5cc0877
Emil Maran <maran.emil@gmail.com>
2019-02-07 17:20:48 +0100

    howto

--- fullstack/php/phpoffice_phpexcel_create_xls_csv.php
+++ fullstack/php/phpoffice_phpexcel_create_xls_csv.php
@@ -124,3 +124,45 @@ $sheet->setCellValue('A1', 'Hello World !');
 $writer = new Xlsx($spreadsheet);
 $writer->save('hello world.xlsx');
 */
+
+
+/*
+#########################################################
+
+Export as Excel5 Format Force Download
+https://hotexamples.com/examples/-/PHPExcel_IOFactory/createWriter/php-phpexcel_iofactory-createwriter-method-examples.html
+
+#########################################################
+*/
+
+
+//load our new PHPExcel library
+$this->load->library('excel');
+//activate worksheet number 1
+$this->excel->setActiveSheetIndex(0);
+//name the worksheet
+$this->excel->getActiveSheet()->setTitle('test worksheet');
+//set cell A1 content with some text
+$this->excel->getActiveSheet()->setCellValue('A1', 'This is just some text value');
+//change the font size
+$this->excel->getActiveSheet()->getStyle('A1')->getFont()->setSize(20);
+//make the font become bold
+$this->excel->getActiveSheet()->getStyle('A1')->getFont()->setBold(true);
+//merge cell A1 until D1
+$this->excel->getActiveSheet()->mergeCells('A1:D1');
+//set aligment to center for that merged cell (A1 to D1)
+$this->excel->getActiveSheet()->getStyle('A1')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+
+$filename = 'just_some_random_name.xls';
+//save our workbook as this file name
+header('Content-Type: application/vnd.ms-excel');
+//mime type
+header('Content-Disposition: attachment;filename="' . $filename . '"');
+//tell browser what's the file name
+header('Cache-Control: max-age=0');
+//no cache
+//save it to Excel5 format (excel 2003 .XLS file), change this to 'Excel2007' (and adjust the filename extension, also the header mime type)
+//if you want to save it as .XLSX Excel 2007 format
+$objWriter = PHPExcel_IOFactory::createWriter($this->excel, 'Excel5');
+//force user to download the Excel file without writing it to server's HD
+$objWriter->save('php://output');
