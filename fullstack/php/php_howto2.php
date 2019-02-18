1b8bf2a45609f4c2251ec9b4265ba232ce17372b
Emil Maran <maran.emil@gmail.com>
2019-02-18 15:17:09 +0100

    howto

--- fullstack/php/php_howto2.php
+++ fullstack/php/php_howto2.php
@@ -1118,3 +1118,51 @@ print_r($ini_array);
 
 
 
+##########################################################
+#
+#	Klassen und Objekte
+#	http://php.net/manual/de/language.oop5.php
+#	http://php.net/manual/en/language.oop5.basic.php
+#
+##########################################################
+
+class Classy {
+
+const       STAT = 'S' ; // no dollar sign for constants (they are always static)
+static     $stat = 'Static' ;
+public     $publ = 'Public' ;
+private    $priv = 'Private' ;
+protected  $prot = 'Protected' ;
+
+function __construct( ){  }
+
+public function showMe( ){
+    print '<br> self::STAT: '  .  self::STAT ; // refer to a (static) constant like this
+    print '<br> self::$stat: ' . self::$stat ; // static variable
+    print '<br>$this->stat: '  . $this->stat ; // legal, but not what you might think: empty result
+    print '<br>$this->publ: '  . $this->publ ; // refer to an object variable like this
+    print '<br>' ;
+}
+}
+$me = new Classy( ) ;
+$me->showMe( ) ;
+
+##########################################################
+#
+#	Verwendung von instanceof mit ererbten Klassen
+#	Checks if the object is of this class or has this class as one of its parents
+#	http://php.net/manual/de/internals2.opcodes.instanceof.php
+#	http://php.net/manual/de/language.operators.type.php
+#	http://php.net/manual/de/function.is-a.php
+#
+##########################################################
+
+class WidgetFactory {   var $oink = 'moo'; }
+$WF = new WidgetFactory();
+
+if (is_a($WF, 'WidgetFactory')) {
+  echo "yes, \$WF is still a WidgetFactory\n";
+}
+if ($WF instanceof WidgetFactory) {
+    echo 'Yes, $WF is a WidgetFactory';
+}
