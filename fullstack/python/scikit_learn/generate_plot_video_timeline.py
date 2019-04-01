3d2d6541274e43c9af8603ba48517b436baa4b45
Emil Maran <maran.emil@gmail.com>
2019-04-01 22:00:33 +0200

    howto

---
+++ fullstack/python/scikit_learn/generate_plot_video_timeline.py
@@ -0,0 +1,34 @@
+
+import matplotlib.pyplot as plt
+y_values = ['Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Jake Tapper', 'Trevor Noah', 'Trevor Noah', 'Trevor Noah', 'Trevor Noah', 'Trevor Noah', 'Trevor Noah', 'Trevor Noah', 'Trevor Noah', 'Trevor Noah', 'Trevor Noah', 'Trevor Noah', 'Trevor Noah', 'Desi Lydic', 'Desi Lydic', 'Desi Lydic', 'Desi Lydic', 'Desi Lydic', 'Ronny Chieng', 'Ronny Chieng', 'Dulcé Sloan', 'Dulcé Sloan', 'Dulcé Sloan', 'Dulcé Sloan', 'Dulcé Sloan', 'Dulcé Sloan', 'Roy Wood Jr.', 'Roy Wood Jr.', 'Roy Wood Jr.', 'Roy Wood Jr.', 'Roy Wood Jr.', ' Young-White', ' Young-White', ' Young-White', ' Young-White', ' Young-White', 'Michael Kosta', 'Michael Kosta', 'Michael Kosta', 'Michael Kosta' ]
+x_values = [7, 13, 11, 2, 33, 47, 8, 14, 26, 18, 15, 12, 3, 17, 7, 40, 4, 12, 16, 15, 48, 13, 11, 18, 15, 18, 4, 24, 13, 11, 36, 35, 2, 236, 31, 249, 179, 112, 18, 2, 136, 12, 13, 117, 70, 108, 36, 48, 5, 18, 64, 37, 35, 8, 123, 23, 28, 41, 33, 26, 16, 26, 11, 108, 68 ]
+# Customize plot.
+plt.figure(figsize=(8, 6))
+plt.title("The Daily Show News Team Live | SXSW 2019", fontsize=10)
+plt.xlabel('speakers’ interventions in seconds', fontsize=10)
+plt.ylabel('Guests', fontsize=10)
+plt.tick_params(axis='both', labelsize=9)
+plt.axis([0, 260, -1, 8])
+plt.scatter(x_values, y_values, s=50, color='r', linewidth=0.5)
+plt.savefig("graph.png")
+# https://repl.it/languages/python3
+# http://phptester.net/
+
+####################################################################
+
+import matplotlib.pyplot as plt
+
+names = ['Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Jake ', 'Trevor ', 'Trevor ', 'Trevor ', 'Trevor ', 'Trevor ', 'Trevor ', 'Trevor ', 'Trevor ', 'Trevor ', 'Trevor ', 'Trevor ', 'Trevor ', 'Desi ', 'Desi ', 'Desi ', 'Desi ', 'Desi ', 'Ronny ', 'Ronny ', 'Dulcé Sloan', 'Dulcé Sloan', 'Dulcé Sloan', 'Dulcé Sloan', 'Dulcé Sloan', 'Dulcé Sloan', 'Roy ', 'Roy ', 'Roy ', 'Roy ', 'Roy ', ' Young-White', ' Young-White', ' Young-White', ' Young-White', ' Young-White', 'Michael ', 'Michael ', 'Michael ', 'Michael ' ]
+values = [7, 13, 11, 2, 33, 47, 8, 14, 26, 18, 15, 12, 3, 17, 7, 40, 4, 12, 16, 15, 48, 13, 11, 18, 15, 18, 4, 24, 13, 11, 36, 35, 2, 236, 31, 249, 179, 112, 18, 2, 136, 12, 13, 117, 70, 108, 36, 48, 5, 18, 64, 37, 35, 8, 123, 23, 28, 41, 33, 26, 16, 26, 11, 108, 68 ]
+
+plt.figure(figsize=(12, 6))
+plt.figure(1,figsize=(26, 6))
+#plt.figure(1, figsize=(26, 6))
+plt.subplot(131)
+plt.bar(names, values)
+plt.subplot(132)
+plt.scatter(names, values,s=50, color='r', linewidth=0.9)
+plt.subplot(133)
+plt.plot(names, values, color='g')
+plt.suptitle('Categorical Plotting')
+plt.savefig("graph.png")
