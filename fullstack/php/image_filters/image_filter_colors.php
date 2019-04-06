c8d5badcff3f2476b2d9dec3785a27088bb58fab
Emil Maran <maran.emil@gmail.com>
2019-04-06 19:11:26 +0200

    howto

---
+++ fullstack/php/image_filters/image_filter_colors.php
@@ -0,0 +1,87 @@
+<?php
+$image = imagecreatefromjpeg('pexels-photo-1910014.jpg');
+for($ias = 0; $ias < 12; $ias++){
+    imagefilter($image, IMG_FILTER_BRIGHTNESS, -$ias); # -100/+100
+    imagefilter($image, IMG_FILTER_CONTRAST, -$ias); # -50/+50
+    #imagefilter($image, IMG_FILTER_GRAYSCALE);
+    imagejpeg($image, "output/img_filter_{$ias}.jpg");
+}
+imagedestroy($image);
+
+$strHTMLIMG='';
+for($ias = 0; $ias < 12; $ias++){
+    $strHTMLIMG.='<div style="float: left; margin: 5px">';
+    $strHTMLIMG.='<img src="output/img_filter_'.$ias.'.jpg" class="plotimg" />';
+    $strHTMLIMG.='</div>';
+}
+$strHTML = '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+    <style>
+        body {
+        background: #20262E;
+        padding: 20px;
+        }
+        .plotimg {
+        width: 255px;
+        }
+    </style>
+
+</head>
+<body>
+
+<div class="container">
+    <div class="col-md-12">
+    '.$strHTMLIMG.'
+    </div>
+</div>
+
+</body>
+</html>
+';
+
+file_put_contents(__FILE__.".html",$strHTML);
+
+
+
+/*
+imagefilter($image, IMG_FILTER_COLORIZE, 100, 0, 0); # -100,-100,-100 - 100,100,100
+imagejpeg($image, 'img_filter_colorize_100_0_0.jpg');
+imagefilter($image, IMG_FILTER_EDGEDETECT);
+imagefilter($image, IMG_FILTER_GRAYSCALE);
+imagefilter($image, IMG_FILTER_EMBOSS);
+imagefilter($image, IMG_FILTER_GAUSSIAN_BLUR);
+imagefilter($image, IMG_FILTER_SELECTIVE_BLUR);
+imagefilter($image, IMG_FILTER_MEAN_REMOVAL);
+imagefilter($image, IMG_FILTER_SMOOTH, 5);
+imagefilter($image, IMG_FILTER_NEGATE);
+*/
+
+
+/*
+
+
+
+https://davidwalsh.name/php-image-filter
+https://www.phpied.com/image-fun-with-php-part-2/
+https://www.php.net/manual/de/function.imagefilter.php
+https://pixabay.com/de/users/free-photos-242387/
+https://pixabay.com/de/photos/artischocken-gem%C3%BCse-gourmet-1246858/
+https://de.freeimages.com/
+https://de.freeimages.com/photo/lake-at-the-cottage-1372381
+https://www.freepik.com/popular-photos
+https://www.freepik.com/free-photo/wooden-board-empty-table-top-blurred-background_1387629.htm
+https://unsplash.com/
+https://unsplash.com/photos/Q9YdEzqwfDE
+https://unsplash.com/photos/qkWmW-70M9Y
+https://stocksnap.io/
+https://stocksnap.io/photo/XQESGTCJEV
+https://www.pexels.com/photo/woman-standing-against-wall-wearing-yellow-turtleneck-sweater-1910014/
+https://www.pexels.com/
+*/
+
+
--- fullstack/php/statistics_machine_learning/extractig_top_colors_images.php
+++ fullstack/php/statistics_machine_learning/extractig_top_colors_images.php
@@ -1,12 +1,10 @@
 <?php
-
 // File and new size
-$filename = 'test.jpg';
+#$filename = 'artichokes-1246858_960_720.jpg';
+$filename = 'lake-at-the-cottage-1372381.jpg';
 $percent = 0.05;
-
 // Content type
 header('Content-Type: image/jpeg');
-
 // Get new sizes
 list($width, $height) = getimagesize($filename);
 $newWidth = $width * $percent;
@@ -57,16 +55,11 @@ $average = array_sum($arReference)/count($arReference);
 echo "Average:".$average.PHP_EOL;
 $max = max($arReference);
 echo "Max: ".$max.PHP_EOL;
-
 echo "---------------------------------".PHP_EOL;
 echo "Top 5 hex colors: ".PHP_EOL;
 arsort($countHex);
 $top5 = array_slice($countHex,0, 5);
 print_r($top5);
-//$keys = array_keys($countHex);
-//print_r($keys);
-//print PHP_EOL;
-//print_r( key($top5)); // hex color nr 1
 echo "---------------------------------".PHP_EOL;
 // print_r( hexdec( key($top5))); //
 echo "Top color: ".PHP_EOL;
@@ -74,73 +67,18 @@ list($r, $g, $b) = sscanf(key($top5), "#%02x%02x%02x");
 echo key($top5)."-> $r $g $b"; // rgb color nr 1
 print PHP_EOL;
 
-/*
-$three = array_chunk($n, 3, true)[0];
-$three = array_slice($n, 0, 3, true);
-*/
+// print colors in terminal
+// exec("printf '\e]4;1;%s\a\e[0;41m \n\e[m' '#ff0000'");
+// xlogo -bg '#ff0000'
+// https://misc.flogisoft.com/bash/tip_colors_and_formatting
 
-/*
-
-https://www.php.net/manual/de/function.krsort.php
-https://www.php.net/manual/de/function.arsort.php
-https://www.php.net/manual/de/function.array-count-values.php
-https://www.php.net/manual/de/function.array-values.php
-https://unsplash.com/search/photos/blue-sky
-https://www.php.net/manual/en/function.imagescale.php
-https://www.php.net/manual/en/function.imagecopyresized.php
-https://www.php.net/manual/en/function.imagecopyresized.php
-https://www.php.net/manual/en/function.imagecopyresampled.php
-
-https://canvasjs.com/php-charts/scatter-point-chart/
-https://jpgraph.net/download/manuals/chunkhtml/ch15s05.html
-https://jpgraph.net/
-https://www.codediesel.com/php/6-excellent-charting-libraries-for-php/
-https://www.edrawsoft.com/createscatterchart.php
-https://demo.koolphp.net/Examples/KoolChart/ChartTypes/Scatter_Chart/index.php
-https://www.fusioncharts.com/dev/chart-guide/standard-charts/select-scatter-chart
-https://www.zingchart.com/docs/chart-types/scatter-plots/
-http://pchart.sourceforge.net/documentation.php?topic=exemple25
-https://www.highcharts.com/demo/scatter
-http://www.jzy3d.org/tutorial.php
-
-
-https://www.python-kurs.eu/matplotlib.php
-
-https://matplotlib.org/users/colors.html
-https://github.com/matplotlib/matplotlib/issues/5377
-https://stackoverflow.com/questions/33287156/specify-color-of-each-point-in-scatter-plot-matplotlib
-https://jakevdp.github.io/PythonDataScienceHandbook/04.02-simple-scatter-plots.html
-https://stackoverflow.com/questions/12236566/setting-different-color-for-each-series-in-scatter-plot-on-matplotlib
-https://community.plot.ly/t/specifying-a-color-for-each-point-in-a-3d-scatter-plot/12652
-https://franciscouzo.github.io/image_colors/
-https://en.wikipedia.org/wiki/CIELAB_color_space
-https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.plot.scatter.html
-https://www.r-graph-gallery.com/121-manage-colors-with-plotly/
-https://gist.github.com/Uberi/4885a318e7ef2afa7f22
-https://kite.com/python/docs/matplotlib.pyplot.scatter
-https://stackoverflow.com/questions/12182891/plot-image-color-histogram-using-matplotlib
-https://matplotlib.org/api/_as_gen/matplotlib.pyplot.show.html
-https://www.quora.com/How-do-I-generate-n-visually-distinct-RGB-colours-in-Python
-
-https://pythonspot.com/image-histogram/
-https://www.pyimagesearch.com/2014/01/22/clever-girl-a-guide-to-utilizing-color-histograms-for-computer-vision-and-image-search-engines/
-https://www.bogotobogo.com/python/OpenCV_Python/python_opencv3_image_histogram_calcHist.php
-https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_histograms/py_2d_histogram/py_2d_histogram.html
-https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_histograms/py_histogram_begins/py_histogram_begins.html
-https://matplotlib.org/users/image_tutorial.html
-
-
-*/
-
-
-// find the maximum in the histogram in order to display a normated graph
-/*$max = 0;
-for ($i=0; $i<255; $i++)
-{
-        if ($histo[$i] > $max)
-        {
-                $max = $histo[$i];
-        }
+$strHTML = '<img src="stat.jpg"  width="300px"/>';
+$jsonColors = json_encode($top5);
+$arrColors = json_decode($jsonColors,true);
+foreach($arrColors as $hexColor => $count){
+	$strHTML.= '<div style="background: '.$hexColor.';width:300px"><span style="color:white">'.$hexColor . "( ".$count.')</span></div>';
 }
-echo $max;*/
-?>
+
+file_put_contents(__FILE__.".html",$strHTML);
+imagejpeg($thumb, 'stat.jpg');
+imagedestroy($thumb);
