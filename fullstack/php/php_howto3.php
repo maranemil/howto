f231b881b24f9a4b0fcb21dd24a0e26df319ec93
Emil Maran <maran.emil@gmail.com>
2019-06-28 14:49:29 +0200

    howto

--- fullstack/php/php_howto3.php
+++ fullstack/php/php_howto3.php
@@ -288,3 +288,23 @@ $client = new SOAPClient($wsdl, array('connection_timeout' => 1));
 // quick and dirty
 sleep(15);
 
+
+
+
+
+
+
+###########################
+Basic Usage PHP imagick
+###########################
+https://www.php.net/manual/de/imagick.examples-1.php
+header('Content-type: image/jpeg');
+$image = new Imagick('image.jpg');
+// If 0 is provided as a width or height parameter,
+// aspect ratio is maintained
+$image->thumbnailImage(100, 0);
+echo $image;
+
+
+
+
