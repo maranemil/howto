8d31759ca2ce7d46abda4d4baed97677341d163a
Emil Maran <maran.emil@gmail.com>
2019-09-04 11:07:40 +0200

    howto

--- fullstack/php/php_howto3.php
+++ fullstack/php/php_howto3.php
@@ -536,3 +536,51 @@ $sClient = new mySoap($wsdl, array('trace'=>1));
 $wrapper->word = new SoapVar("Echo testing", XSD_STRING);
 $result = $sClient->echo($wrapper);
 print_r($result->return);
+
+
+// --------------------
+
+ini_set('display_errors', true);
+ini_set('display_startup_errors', true);
+error_reporting(E_ALL);
+require 'soap-wsa.php';
+require 'soap-wsse.php';
+
+class WsseAuthHeader extends SoapHeader {
+
+   private $wss_ns = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd';
+
+   function __construct($user, $pass, $ns = null) {
+       if ($ns) {
+           $this->wss_ns = $ns;
+       }
+       $auth = new stdClass();
+       $auth->Username = new SoapVar($user, XSD_STRING, NULL, $this->wss_ns, NULL, $this->wss_ns);
+       $auth->Password = new SoapVar($pass, XSD_STRING, NULL, $this->wss_ns, NULL, $this->wss_ns);
+       $username_token = new stdClass();
+       $username_token->UsernameToken = new SoapVar($auth, SOAP_ENC_OBJECT, NULL, $this->wss_ns, 'UsernameToken', $this->wss_ns);
+       $security_sv = new SoapVar(
+               new SoapVar($username_token, SOAP_ENC_OBJECT, NULL, $this->wss_ns, 'UsernameToken', $this->wss_ns), SOAP_ENC_OBJECT, NULL, $this->wss_ns, 'Security', $this->wss_ns);
+       parent::__construct($this->wss_ns, 'Security', $security_sv, true);
+   }
+}
+
+$username = 'test';
+$password = 'test123';
+$wsdl = 'https://sop-ws.example.de/sbb/services/Invoke?wsdl';
+
+$wsse_header = new WsseAuthHeader($username, $password);
+$client = new SoapClient($wsdl, array(
+      "trace" => 1,
+      "exceptions" => 0
+   )
+);
+$client->__setSoapHeaders(array($wsse_header));
+$request = array(
+    "functionResponseName" => array(
+        'param1' => "string",
+        "param2" => "string"
+    )
+);
+$results = $client->FunctionName($request);
+var_dump($results);
