73c53bec3052cdc0832e2d321b4e32eedf4d8f5f
Emil Maran <maran.emil@gmail.com>
2019-09-04 23:14:47 +0200

    howto

--- datamining/opencv_machinelearning/datacamp/z3_tensorflow_keras_minimal.py
+++ datamining/opencv_machinelearning/datacamp/z3_tensorflow_keras_minimal.py
@@ -35,3 +35,35 @@ https://codelabs.developers.google.com/codelabs/tensorflow-lab1-helloworld/#5
 https://www.tensorflow.org/js/guide/train_models
 https://github.com/keras-team/keras/issues/741
 """
+
+import tensorflow as tf
+print(tf.__version__)
+
+mnist = tf.keras.datasets.fashion_mnist
+
+(training_images, training_labels) ,  (test_images, test_labels) = mnist.load_data()
+
+training_images = training_images/255.0
+test_images = test_images/255.0
+"""
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(1024, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+"""
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+model.compile(optimizer = 'adam',
+              loss = 'sparse_categorical_crossentropy')
+
+model.fit(training_images, training_labels, epochs=5)
+model.evaluate(test_images, test_labels)
+classifications = model.predict(test_images)
+print(classifications[0])
+print(test_labels[0])
+
+
+"""
+https://colab.research.google.com/github/lmoroney/mlday-tokyo/blob/master/Lab2-Computer-Vision.ipynb#scrollTo=7mAyndG3kVlK
+"""
