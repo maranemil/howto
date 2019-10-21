2cf5ca6be60ce6cc29c72e91309e783fa4044b1f
Emil Maran <maran.emil@gmail.com>
2019-10-21 20:44:51 +0200

    howto

--- datamining/machine_learning/faster-r-cnn-python-object-detection.py
+++ datamining/machine_learning/faster-r-cnn-python-object-detection.py
@@ -120,3 +120,99 @@ python test_frcnn.py - p test_images
 
 
 
+
+
+
+
+
+
+"""
+Loading in your own data - Deep Learning basics with Python, TensorFlow and Keras p.2
+https://www.youtube.com/watch?v=j-3vuBynnOE
+https://pythonprogramming.net/loading-custom-data-deep-learning-python-tensorflow-keras/
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+from tqdm import tqdm
+
+DATADIR = "X:/Datasets/PetImages"
+
+CATEGORIES = ["Dog", "Cat"]
+
+for category in CATEGORIES:  # do dogs and cats
+    path = os.path.join(DATADIR,category)  # create path to dogs and cats
+    for img in os.listdir(path):  # iterate over each image per dogs and cats
+        img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
+        plt.imshow(img_array, cmap='gray')  # graph it
+        plt.show()  # display!
+
+        break  # we just want one for now so break
+    break  #...and one more!
+print(img_array)
+print(img_array.shape)
+
+IMG_SIZE = 50
+
+new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array, cmap='gray')
+plt.show()
+
+new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array, cmap='gray')
+plt.show()
+
+training_data = []
+
+def create_training_data():
+    for category in CATEGORIES:  # do dogs and cats
+
+        path = os.path.join(DATADIR,category)  # create path to dogs and cats
+        class_num = CATEGORIES.index(category)  # get the classification  (0 or a 1). 0=dog 1=cat
+
+        for img in tqdm(os.listdir(path)):  # iterate over each image per dogs and cats
+            try:
+                img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
+                training_data.append([new_array, class_num])  # add this to our training_data
+            except Exception as e:  # in the interest in keeping the output clean...
+                pass
+            #except OSError as e:
+            #    print("OSErrroBad img most likely", e, os.path.join(path,img))
+            #except Exception as e:
+            #    print("general exception", e, os.path.join(path,img))
+
+create_training_data()
+print(len(training_data))
+
+
+import random
+random.shuffle(training_data)
+for sample in training_data[:10]:
+    print(sample[1])
+
+X = []
+y = []
+for features,label in training_data:
+    X.append(features)
+    y.append(label)
+print(X[0].reshape(-1, IMG_SIZE, IMG_SIZE, 1))
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+
+import pickle
+pickle_out = open("X.pickle","wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+pickle_out = open("y.pickle","wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+
+
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
