22cf5819edf24c47903a7cc3b29dc3458db49e84
Emil Maran <maran.emil@gmail.com>
2019-11-07 15:48:07 +0100

    howto

---
+++ fullstack/php/snippets/php_howto4.php
@@ -0,0 +1,60 @@
+####################################################
+#
+# PHP CACHE Options
+#
+####################################################
+
+/*
+https://www.php.net/manual/en/memcache.set.php
+http://www.php-cache.com/en/latest/
+https://github.com/php-cache/predis-adapter
+https://www.php.net/manual/de/book.opcache.php
+https://www.php.net/manual/de/book.apc.php
+https://www.php.net/manual/de/mysqlnd-qc.quickstart.caching.php
+https://www.php.net/manual/en/book.apc.php
+https://www.doctrine-project.org/projects/doctrine-orm/en/2.6/reference/caching.html
+https://twig.symfony.com/doc/2.x/api.html
+https://www.php.net/manual/en/function.apc-add.php
+https://www.php.net/manual/en/function.apc-store.php
+https://laravel.com/docs/5.7/cache
+https://www.php.net/manual/de/mysqlnd-qc.constants.php
+https://www.hgb-leipzig.de/~uklaus/PHP/mysqlnd-qc.quickstart.caching.html
+https://www.hgb-leipzig.de/~uklaus/PHP/mysqlnd-qc.constants.html
+https://www.php.net/manual/en/book.mysqlnd-qc.php
+
+
+// -----------------------------------------------
+http://www.php-cache.com/en/latest/
+https://www.phpfastcache.com/
+https://www.php-fig.org/psr/psr-6/
+// -----------------------------------------------
+
+[Regular drivers]
+Apc
+Cookie
+Files
+Leveldb
+Memcache
+Sqlite
+Wincache
+Xcache
+Zend Disk Cache
+
+[High performances drivers]
+Cassandra
+CouchBase
+Couchdb
+Mongodb
+Predis
+Redis
+Riak
+Ssdb
+Zend Memory Cache
+
+[Development drivers]
+Devnull
+Devfalse
+Devtrue
+Memstatic
+
+*/
