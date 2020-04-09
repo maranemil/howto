325ec6ed195b06fd259d6e11730f211b84f6584a
Emil Maran <maran.emil@gmail.com>
2020-04-09 11:17:03 +0200

    howto

--- datamining/machine_learning/machine_learning_recipes_youtube.py
+++ datamining/machine_learning/machine_learning_recipes_youtube.py
@@ -11,11 +11,12 @@ https://www.youtube.com/watch?v=cKxRvEZd3Mw&list=PLOU2XLYxmsIIuiBfYad6rFYQU_jL2r
 
 
 from sklearn import tree
-features = [[140,1],[130,1],[150,0],[170,0]]
-labels = [0,0,1,1]
+
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+labels = [0, 0, 1, 1]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
-print(clf.predict([[160,0]]))
+print(clf.predict([[160, 0]]))
 
 """
 weight 	texture	label
@@ -25,23 +26,21 @@ weight 	texture	label
 130g	smooth	apple
 """
 
-
-
 # https://www.youtube.com/watch?v=tNa99PG8hR8 Visualizing a Decision Tree - Machine Learning Recipes
 
 from sklearn.datasets import load_iris
 from sklearn import tree
 
 iris = load_iris()
-print (iris.feature_names)
-print (iris.target_names)
-print (iris.data[0])
-print (iris.target[0])
+print(iris.feature_names)
+print(iris.target_names)
+print(iris.data[0])
+print(iris.target[0])
 
 for i in range(len(iris.target)):
-    print ("Ex %d: label %s, feat %s " % (i, iris.target[i], iris.data[i]))
+    print("Ex %d: label %s, feat %s " % (i, iris.target[i], iris.data[i]))
 
-test_idx = [0,50,100]
+test_idx = [0, 50, 100]
 # training data
 train_target = np.delete(iris.target, test_idx)
 train_data = np.delete(iris.data, test_idx, axis=0)
@@ -49,15 +48,15 @@ train_data = np.delete(iris.data, test_idx, axis=0)
 test_target = iris.target[test_idx]
 test_data = iris.data[test_idx]
 
-
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(train_data, train_target)
-print (test_target)
-print (clf.predict(test_data))
+print(test_target)
+print(clf.predict(test_data))
 # tree.export_graphviz(clf,   out_file='tree2.dot')
 
 from sklearn.externals.six import StringIO
 import pydot
+
 dot_data = StringIO()
 tree.export_graphviz(
     clf,
@@ -94,9 +93,6 @@ graph
 # https://www.youtube.com/watch?v=TF1yh5PKaqI  Getting Started with Weka - Machine Learning Recipes #10
 
 
-
-
-
 # https://www.youtube.com/watch?v=fNxaJsNG3-s  Natural Language Processing - Tokenization (NLP Zero to Hero, part 1)
 # https://www.youtube.com/watch?v=Y_hzMnRXjhI  Training a model to recognize sentiment in text (NLP Zero to Hero, part 3)
 
@@ -109,8 +105,39 @@ graph
 # https://www.youtube.com/watch?v=BO4g2DRvL6U  Prepare your data for ML | Text Classification Tutorial Pt. 1 (Coding TensorFlow)
 
 
-
 # https://www.youtube.com/watch?v=Y_hzMnRXjhI&list=PLQY2H8rRoyvwLbzbnKJ59NkZvQAW9wLbx
 
+"""
+TensorFlow high-level APIs: Part 1 - loading data           https://www.youtube.com/watch?v=oFFbKogYdfc
+Saving and Loading Models (Coding TensorFlow)               https://www.youtube.com/watch?v=HxtBIwfy0kM
+Use TensorFlow to classify clothing images (Coding TensorFlow) https://www.youtube.com/watch?v=FiNglI1wRNk
+Intro to Machine Learning (ML Zero to Hero - Part 1)        https://www.youtube.com/watch?v=KNAWp2S3w94
+Build an image classifier (ML Zero to Hero - Part 4)        https://www.youtube.com/watch?v=u2TjZzNuly8
+Using TensorBoard with Keras (TensorFlow Tip of the Week)   https://www.youtube.com/watch?v=2U6Jl7oqRkM
+"""
+
+import ternsorflow as tf
+tf.enable_eager_execution()
+#tf.__version___
+
+a = tf.constnt(5)
+b = a + 3
+print(b)
+
+defaults = [tf.int32] * 55
+dataset = tf.contrib.dataCsvDataset(['convtype.csv.train'], detaults)
+print(list(dataset.take(1)))
+
+col_names = ['elevation', 'aspect', ...]
 
 
+def _parse_csv_row(*vals):
+    soil_type_t = tf.convert_to_tensor(vals[14:54])  # combine all vals in one tensor
+    feat_vals = vals[:10] + (soil_type_t, vals[54])
+    features = dict(zip(col_names, feat_vals))
+    class_label = tf.argmax(row_vals[10, 14], axis=8)
+    return features, class_label
+
+
+dataset = dataset.map(_parse_csv_row).batch(64)
+print(list(dataset.take(1)))
