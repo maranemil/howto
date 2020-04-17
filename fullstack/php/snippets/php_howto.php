096aab8e09189d6d1de061dc77a71981a52600f1
Emil Maran <maran.emil@gmail.com>
2020-04-17 19:30:48 +0200

    howto

--- fullstack/php/snippets/php_howto.php
+++ fullstack/php/snippets/php_howto.php
@@ -981,6 +981,46 @@ ini_set("soap.wsdl_cache_enabled", "0");
 ini_set('soap.wsdl_cache_ttl', 0);
 header("Strict-Transport-Security:max-age=63072000");
 
+
+############################################################
+# error_reporting
+############################################################
+/*
+https://www.php.net/manual/de/errorfunc.constants.php
+https://www.peterkropff.de/tutorials/php_fehler/error_reporting.htm
+https://www.w3schools.com/php/func_error_reporting.asp
+https://www.php.net/manual/de/function.error-reporting.php
+*/
+
+/*
+// Error Reporting komplett abschalten
+error_reporting(0);
+
+// Nur einfache Fehler melden
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+// E_NOTICE ist sinnvoll um uninitialisierte oder
+// falsch geschriebene Variablen zu entdecken
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+
+// Melde alle Fehler außer E_NOTICE
+error_reporting(E_ALL & ~E_NOTICE);
+
+// Melde alle PHP Fehler (siehe Changelog)
+error_reporting(E_ALL);
+
+// Melde alle PHP Fehler
+error_reporting(-1);
+
+// Dies entspricht error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+*/
+
+
+
 /*https://www.phpgangsta.de/hsts-http-strict-transport-security-hasts-schon
 https://scotthelme.co.uk/issuing-hsts-policy-in-php/
 https://support.servertastic.com/knowledgebase/article/http-strict-transport-security-php*/
