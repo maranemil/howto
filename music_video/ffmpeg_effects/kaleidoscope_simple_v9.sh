9b0a2c32a51c41d8a27c0d426c00e0cfe07ca1bf
maranemil <maran.emil@gmail.com>
2021-03-13 15:23:01 +0100

    howto

--- music_video/ffmpeg_effects/kaleidoscope_simple_v9.sh
+++ music_video/ffmpeg_effects/kaleidoscope_simple_v9.sh
@@ -24,7 +24,7 @@ ffmpeg -i output1a.mp4  -i output1a.mp4  -filter_complex "[0]pad=iw:ih*2[bg]; [b
 ffmpeg -i output1b.mp4 -i output1b.mp4 -filter_complex  "hflip,[1] fade=in:1:1:alpha=1, fade=out:5000:1:alpha=1, scale=880:880, rotate=-1.45:c=none [ov1];[1] fade=in:1:1:alpha=1, fade=out:5000:1:alpha=1, scale=1280:1280, rotate=1.45:c=none [ov2]; [0][ov1] overlay=300:300 [v1]; [v1][ov2] overlay=W-680:100 [v] " -map "[v]"  -y  -t 10  outlayer2.mp4
 
 
-ffmpeg -i outlayer1.mp4 -vf rotate=PI/8,rotate=PI/270,"split [main][tmp]; [tmp] crop=iw:ih/2:0:0, vflip [flip]; [main][flip] overlay=0:H/2","crop=iw/1.2:ih:0:0,split[left][tmp];[tmp]hflip[right];[left][right] hstack",scale=1560:360 -y  -t 10 output2.mp4
+ffmpeg -i outlayer2.mp4 -vf rotate=PI/8,rotate=PI/270,"split [main][tmp]; [tmp] crop=iw:ih/2:0:0, vflip [flip]; [main][flip] overlay=0:H/2","crop=iw/1.2:ih:0:0,split[left][tmp];[tmp]hflip[right];[left][right] hstack",scale=1560:360 -y  -t 10 output2.mp4
 
 # add saturation
 ffmpeg -i output2.mp4 -vf eq=saturation=1.5 -y output2a.mp4
