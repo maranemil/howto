e4ca8c34682e4147c9fcaf4a119b6198e8eda114
maranemil <maran.emil@gmail.com>
2021-05-12 18:36:01 +0200

    howto

---
+++ fullstack/python/tensorflow/examples/tensorflow_python_keras.py
@@ -0,0 +1,309 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+tf.config.threading.set_inter_op_parallelism_threads(2)
+tf.config.threading.set_intra_op_parallelism_threads(2)
+
+
+# Load the data: the Cats vs Dogs dataset
+
+"""
+!curl -O https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_3367a.zip
+!unzip -q kagglecatsanddogs_3367a.zip
+!ls
+!ls PetImages
+"""
+
+#with tf.device('/CPU:0'):#
+
+
+# Filter out corrupted images
+
+import os
+
+num_skipped = 0
+for folder_name in ("Cat", "Dog"):
+    folder_path = os.path.join("PetImages", folder_name)
+    for fname in os.listdir(folder_path):
+        fpath = os.path.join(folder_path, fname)
+        try:
+            fobj = open(fpath, "rb")
+            is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
+        finally:
+            fobj.close()
+
+        if not is_jfif:
+            num_skipped += 1
+            # Delete corrupted image
+            os.remove(fpath)
+
+print("Deleted %d images" % num_skipped)
+
+# Generate a Dataset
+
+image_size = (60, 60) # 180 180 
+batch_size = 32
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "PetImages",
+    validation_split=0.2,
+    subset="training",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "PetImages",
+    validation_split=0.2,
+    subset="validation",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+
+# Visualize the data
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(int(labels[i]))
+        plt.axis("off")
+
+# Using image data augmentation
+
+data_augmentation = keras.Sequential(
+    [
+        layers.experimental.preprocessing.RandomFlip("horizontal"),
+        layers.experimental.preprocessing.RandomRotation(0.1),
+    ]
+)
+
+plt.figure(figsize=(10, 10))
+for images, _ in train_ds.take(1):
+    for i in range(9):
+        augmented_images = data_augmentation(images)
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(augmented_images[0].numpy().astype("uint8"))
+        plt.axis("off")
+
+
+# Standardizing the data
+# Two options to preprocess the data
+
+#inputs = keras.Input(shape=input_shape)
+#x = data_augmentation(inputs)
+#x = layers.experimental.preprocessing.Rescaling(1./255)(x)
+#...  # Rest of the model
+
+#augmented_train_ds = train_ds.map(
+#  lambda x, y: (data_augmentation(x, training=True), y))
+
+# Configure the dataset for performance
+train_ds = train_ds.prefetch(buffer_size=32)
+val_ds = val_ds.prefetch(buffer_size=32)
+
+# Build a model
+
+def make_model(input_shape, num_classes):
+    inputs = keras.Input(shape=input_shape)
+    # Image augmentation block
+    x = data_augmentation(inputs)
+
+    # Entry block
+    x = layers.experimental.preprocessing.Rescaling(1.0 / 255)(x)
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.Conv2D(64, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    for size in [128, 256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    if num_classes == 2:
+        activation = "sigmoid"
+        units = 1
+    else:
+        activation = "softmax"
+        units = num_classes
+
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(units, activation=activation)(x)
+    return keras.Model(inputs, outputs)
+
+
+model = make_model(input_shape=image_size + (3,), num_classes=2)
+keras.utils.plot_model(model, show_shapes=True)
+
+# Train the model
+
+epochs = 5 # 50 
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
+]
+model.compile(
+    optimizer=keras.optimizers.Adam(1e-3),
+    loss="binary_crossentropy",
+    metrics=["accuracy"],
+)
+model.fit(
+    train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds,
+)
+
+
+# Run inference on new data
+
+img = keras.preprocessing.image.load_img(
+    "PetImages/Cat/6779.jpg", target_size=image_size
+)
+img_array = keras.preprocessing.image.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+predictions = model.predict(img_array)
+score = predictions[0]
+print(
+    "This image is %.2f percent cat and %.2f percent dog."
+    % (100 * (1 - score), 100 * score)
+)
+
+
+"""
+ImportError: ('You must install pydot (`pip install pydot`)
+ and install graphviz (see instructions at https://graphviz.gitlab.io/download/) ',
+ 'for plot_model/model_to_dot to work.')
+
+pip install pydot
+pip install graphviz
+
+sudo pip3 install pydot
+sudo pip3 install graphviz
+
+sudo apt install graphviz
+
+
+How can I reduce the number of CPUs used by Tensorlfow/Keras?
+https://stackoverflow.com/questions/57925061/how-can-i-reduce-the-number-of-cpus-used-by-tensorlfow-keras
+https://stackoverflow.com/questions/40690598/can-keras-with-tensorflow-backend-be-forced-to-use-cpu-or-gpu-at-will
+
+
+import tensorflow as tf
+
+tf.config.threading.set_intra_op_parallelism_threads(2)
+tf.config.threading.set_inter_op_parallelism_threads(2)
+with tf.device('/CPU:0'):
+    model = tf.keras.models.Sequential([...
+
+
+or
+
+import tensorflow as tf
+from keras import backend as K
+
+num_cores = 4
+num_CPU = 1
+num_GPU = 0
+
+config = tf.ConfigProto(intra_op_parallelism_threads=num_cores,
+                        inter_op_parallelism_threads=num_cores, 
+                        allow_soft_placement=True,
+                        device_count = {'CPU' : num_CPU,
+                                        'GPU' : num_GPU}
+                       )
+
+session = tf.Session(config=config)
+K.set_session(session)
+
+or
+
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+#os.environ["CUDA_VISIBLE_DEVICES"] = ""
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+or
+
+import tensorflow as tf
+# your code here
+with tf.device('/gpu:0'):
+    model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# https://keras.io/examples/vision/image_classification_from_scratch/
+
+
