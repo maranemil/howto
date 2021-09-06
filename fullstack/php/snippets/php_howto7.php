3708aea27c1e4d52bb900c6bd2cd757b900ca8ca
maranemil <maran.emil@gmail.com>
2021-09-06 19:00:04 +0200

    howto

---
+++ fullstack/php/snippets/php_howto7.php
@@ -0,0 +1,38 @@
+<?php
+
+#######################################################
+#
+#   Filters
+#
+#######################################################
+
+// echo "<pre>";
+// http://phptester.net/
+
+// https://www.php.net/manual/en/function.filter-var.php
+// https://www.php.net/manual/en/filter.filters.validate.php
+// https://www.php.net/manual/en/filter.filters.sanitize.php
+// https://www.w3schools.com/php/filter_sanitize_number_int.asp
+// https://www.php.net/manual/en/function.strip-tags.php
+
+/*
+
+var_dump(filter_var($string, FILTER_SANITIZE_NUMBER_INT));
+var_dump(filter_var($string, FILTER_VALIDATE_INT));
+var_dump(filter_var($string, FILTER_SANITIZE_STRING));
+var_dump(filter_var($string, FILTER_VALIDATE_BOOLEAN));
+var_dump(filter_var($string, FILTER_VALIDATE_DOMAIN));
+var_dump(filter_var($string, FILTER_VALIDATE_URL));
+var_dump(filter_var($string, FILTER_VALIDATE_EMAIL));
+var_dump(filter_var($string, FILTER_VALIDATE_IP));
+
+var_dump(filter_var($string, FILTER_SANITIZE_NUMBER_INT));
+var_dump(filter_var($string, FILTER_SANITIZE_NUMBER_INT));
+var_dump(filter_var($string, FILTER_SANITIZE_STRING));
+var_dump(filter_var($string, FILTER_SANITIZE_ADD_SLASHES));
+var_dump(filter_var($string, FILTER_SANITIZE_STRING));
+var_dump(filter_var($string, FILTER_SANITIZE_URL));
+var_dump(filter_var($string, FILTER_SANITIZE_EMAIL));
+
+
+*/
