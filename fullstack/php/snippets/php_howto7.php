07017895abf82a45d9d3fca226cd041f5a100977
maranemil <maran.emil@gmail.com>
2021-09-10 15:17:28 +0200

    howto

--- fullstack/php/snippets/php_howto7.php
+++ fullstack/php/snippets/php_howto7.php
@@ -36,3 +36,22 @@ var_dump(filter_var($string, FILTER_SANITIZE_EMAIL));
 
 
 */
+
+/*
+Classic sort
+https://christoph-rumpel.com/2020/8/refactoring-php
+https://www.php.net/manual/en/function.array-filter.php
+*/
+
+$users = [
+    [ 'id' => 801, 'name' => 'Peter', 'score' => 505, 'active' => true],
+    [ 'id' => 844, 'name' => 'Mary', 'score' => 704, 'active' => true],
+    [ 'id' => 542, 'name' => 'Norman', 'score' => 104, 'active' => false],
+];
+
+// Requested Result: only active users, sorted by score ["Mary(704)","Peter(505)"]
+$users = array_filter($users, fn ($user) => $user['active']);
+usort($users, fn($a, $b) => $a['score'] < $b['score']);
+$userHighScoreTitles = array_map(fn($user) => $user['name'] . '(' . $user['score'] . ')', $users);
+return $userHighScoreTitles;
+
