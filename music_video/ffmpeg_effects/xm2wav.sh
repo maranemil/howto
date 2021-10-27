08bdd12b1d1c23270a432408f356f231e960688c
Emil Maran <maran.emil@gmail.com>
2021-10-28 00:35:42 +0200

    howto

---
+++ music_video/ffmpeg_effects/xm2wav.sh
@@ -0,0 +1,7 @@
+# Batch converter Files XM to WAV in Ubuntu
+
+# test if play works else install ffmpeg
+# ffplay -i your_track.xm
+
+# convert files from xm to wav
+for i in *.xm ; do ffmpeg  -i "$i" "$i".wav; done
