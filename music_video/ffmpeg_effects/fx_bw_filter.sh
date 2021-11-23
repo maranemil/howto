5c9c486bda88545fdd5298d51d5b9114a65c25d1
Emil Maran <maran.emil@gmail.com>
2021-11-23 19:21:21 +0100

    howto

--- music_video/ffmpeg_effects/fx_bw_filter.sh
+++ music_video/ffmpeg_effects/fx_bw_filter.sh
@@ -5,3 +5,10 @@ ffmpeg -i in1.mp4 -vf lutyuv="u=128:v=128" -y output.mp4
 
 # add saturation
 ffmpeg -i output.mp4 -vf eq=saturation=1.5 -y output2.mp4
+
+# FFmpeg & Black and White Conversion
+# https://stackoverflow.com/questions/32384057/ffmpeg-black-and-white-conversion
+
+ffmpeg -i input.mp4 -vf format=gray output.mp4
+ffmpeg -i input.mp4 -vf hue=s=0 output.mp4
+ffmpeg -i input.mp4 -vf lutyuv=u=128:v=128 output.mp4
--- music_video/ffmpeg_effects/fx_rgbashift.sh
+++ music_video/ffmpeg_effects/fx_rgbashift.sh
@@ -6,3 +6,11 @@ ffplay -i in.mp4 -vf "scale=-2:480,bbox=min_val=2,pad=iw+0:ih+5:0:5:#ffffff,rgba
 # rgbashift
 ffplay -i in.mp4 -vf "scale=-2:480,bbox=min_val=2,pad=iw+0:ih+5:0:5:#ffffff,rgbashift=rh=50:rv=-50:gh=-1:bh=-1:bv=-50:av=10"
 
+#fx
+ffplay -i in.mp4 -vf rgbashift=rv=5:bv=-5
+ffplay -i in.mp4 -vf rgbashift=rv=0:gv=0:bv=0
+ffplay -i in.mp4 -vf rgbashift=gh=20:bh=10:bv=10
+ffplay -i in.mp4 -vf rgbashift=rh=15:bv=15:gh=-15
+ffplay -i in.mp4 -vf rgbashift=rh=-10
+ffplay -i in.mp4 -vf rgbashift=rh=-25:rv=250:gh=-250:gv=5:bh=-250:bv=250:ah=-25:av=250
+
