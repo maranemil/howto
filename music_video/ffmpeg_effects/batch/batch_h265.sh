48815c5c0d47c10a6ac77c497ccce481ab7dd7e0
Emil Maran <maran.emil@gmail.com>
2022-12-29 17:36:46 +0100

    howto

--- music_video/ffmpeg_effects/batch/batch_h265.sh
+++ music_video/ffmpeg_effects/batch/batch_h265.sh
@@ -1,3 +1,5 @@
-# batch
-
+# batch h265
 mkdir conv && for f in *.mkv; do ffmpeg -i $f -c:v libx265 -c:a copy -vtag hvc1 -threads 1 conv/$f.mp4; done
+
+# h264
+mkdir conv && for f in *.mkv; do ffmpeg -i $f -c:a copy -threads 1 conv/$f.mp4; done
