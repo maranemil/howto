4b9dbfef9524c126f2d5fd8afa1e117827bbe16a
Emil Maran <maran.emil@gmail.com>
2022-12-31 16:39:08 +0100

    howto

--- music_video/ffmpeg_effects/batch/batch_h265.sh
+++ music_video/ffmpeg_effects/batch/batch_h265.sh
@@ -3,3 +3,8 @@ mkdir conv && for f in *.mkv; do ffmpeg -i $f -c:v libx265 -c:a copy -vtag hvc1 
 
 # h264
 mkdir conv && for f in *.mkv; do ffmpeg -i $f -c:a copy -threads 1 conv/$f.mp4; done
+
+
+
+# ffmpeg -i in.mkv -vf scale=-1:480 -preset ultrafast -threads 1 out.mp4
+# ffmpeg -i in.mkv -vf scale=-1:480 -c:a copy -threads 1 out2.mp4
