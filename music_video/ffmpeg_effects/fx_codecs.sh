84e6609f83918392242fa38b183faf30b18c358e
Emil Maran <maran.emil@gmail.com>
2023-04-16 10:24:33 +0200

    howto

--- music_video/ffmpeg_effects/fx_codecs.sh
+++ music_video/ffmpeg_effects/fx_codecs.sh
@@ -5,15 +5,22 @@
 # https://stackoverflow.com/questions/3377300/what-are-all-codecs-and-formats-supported-by-ffmpeg
 # https://stackoverflow.com/questions/5678695/ffmpeg-usage-to-encode-a-video-to-h264-codec-format
 # https://superuser.com/questions/785528/how-to-generate-an-mp4-with-h-265-codec-using-ffmpeg
+# https://trac.ffmpeg.org/wiki/Encode/H.265
+# https://x265.readthedocs.io/en/latest/presets.html
+# https://stackoverflow.com/questions/47389381/ffmpeg-multiple-x265-params-are-not-recognized
+# https://x265.readthedocs.io/en/2.5/cli.html
 
 ffmpeg -codecs
 ffmpeg -formats
 ffmpeg -codecs | grep h264
+ffmpeg -h encoder=libx265
 
 ffmpeg -i input.flv -vcodec libx264 -acodec aac -threads 1 output.mp4
-ffmpeg -i INPUT -c:v libx265 -c:a copy -x265-params crf=25 -threads 1 OUT.mov
-ffmpeg -i INPUT -c:v libx265 -c:a copy -threads 1 OUT.mov
+ffmpeg -i INPUT -c:v libx265 -c:a copy -x265-params crf=25 -threads 1 OUT
+ffmpeg -i INPUT -c:v libx265 -c:a copy -threads 1 OUT
 
 # GOPR0377.MP4  2GB
 # libx264       500MB
 # libx265       225MB
+
+# ffmpeg -i INPUT -c:v libx265 -preset medium -tune psnr -x265-params "qp=16:rc-lookahead=18" OUT
