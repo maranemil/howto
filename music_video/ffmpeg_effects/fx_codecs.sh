7302815b9d081a287965af17e3db680923791a45
Emil Maran <maran.emil@gmail.com>
2023-04-16 10:18:24 +0200

    howto

--- music_video/ffmpeg_effects/fx_codecs.sh
+++ music_video/ffmpeg_effects/fx_codecs.sh
@@ -13,3 +13,7 @@ ffmpeg -codecs | grep h264
 ffmpeg -i input.flv -vcodec libx264 -acodec aac -threads 1 output.mp4
 ffmpeg -i INPUT -c:v libx265 -c:a copy -x265-params crf=25 -threads 1 OUT.mov
 ffmpeg -i INPUT -c:v libx265 -c:a copy -threads 1 OUT.mov
+
+# GOPR0377.MP4  2GB
+# libx264       500MB
+# libx265       225MB
