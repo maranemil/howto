34c350d7eb0b8a742b499d7b009ea97a7152981d
Emil Maran <maran.emil@gmail.com>
2023-04-16 10:37:40 +0200

    howto

--- music_video/ffmpeg_effects/fx_codecs.sh
+++ music_video/ffmpeg_effects/fx_codecs.sh
@@ -9,6 +9,8 @@
 # https://x265.readthedocs.io/en/latest/presets.html
 # https://stackoverflow.com/questions/47389381/ffmpeg-multiple-x265-params-are-not-recognized
 # https://x265.readthedocs.io/en/2.5/cli.html
+# https://x265.readthedocs.io/en/latest/cli.html#cmdoption-pools
+# https://x265.readthedocs.io/en/stable/threading.html
 
 ffmpeg -codecs
 ffmpeg -formats
@@ -18,6 +20,10 @@ ffmpeg -h encoder=libx265
 ffmpeg -i input.flv -vcodec libx264 -acodec aac -threads 1 output.mp4
 ffmpeg -i INPUT -c:v libx265 -c:a copy -x265-params crf=25 -threads 1 OUT
 ffmpeg -i INPUT -c:v libx265 -c:a copy -threads 1 OUT
+
+# cpu limiting
+ffmpeg -i GOPR0378.MP4 -c:v libx265 -c:a copy -x265-params pools=2 GOPR0378c.mp4
+
 
 # GOPR0377.MP4  2GB
 # libx264       500MB
