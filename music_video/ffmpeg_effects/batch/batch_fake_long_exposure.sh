b1caced6af1b1a097b5eeac84f9fe5c6909168ea
EM <maran.emil@gmail.com>
2023-10-06 10:14:35 +0200

    howto

--- music_video/ffmpeg_effects/batch/batch_fake_long_exposure.sh
+++ music_video/ffmpeg_effects/batch/batch_fake_long_exposure.sh
@@ -5,9 +5,13 @@
 
 # https://trac.ffmpeg.org/wiki/Blend
 
+# video frame segmentation
 ffmpeg -i in.mp4 -r 1/1 -t 10 $filename%03d.png
 
-
+# mix overlay
 ffmpeg -i 002.png -i 003.png -i 004.png \
 	-filter_complex "[1:0] format=rgba,unsharp=5:5:1.0:5:5:0.0 [1sared]; [0:0]format=rgba,unsharp=3:3:1.5 [0rgbd]; [0rgbd][1sared]blend=all_mode='overlay':shortest=1:all_opacity=0.5" \
 	-y output.jpg
+
+# test results
+ffmpeg -i 010.png -i output_hd2.jpg -i output_Nero_AI_Face_x2x.png -filter_complex hstack=inputs=3 -y z.png
