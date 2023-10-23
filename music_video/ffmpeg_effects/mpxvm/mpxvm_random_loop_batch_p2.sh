d3ada42e2298485398890dd0dc584d37113e250f
EM <maran.emil@gmail.com>
2023-10-23 22:40:49 +0200

    howto

--- music_video/ffmpeg_effects/mpxvm/mpxvm_random_loop_batch_p2.sh
+++ music_video/ffmpeg_effects/mpxvm/mpxvm_random_loop_batch_p2.sh
@@ -62,6 +62,50 @@ ffmpeg -i mpxvm_loops.wav -af silencedetect=d=0.5 -f null - |& awk '/silence_end
 varseg=$(tr '\n' ',' < segments.txt) &&  ffmpeg -i mpxvm_loops.wav -f segment -segment_times ${varseg::-1} -reset_timestamps 1 -map 0:a -c:a copy output_%03d.wav
 
 
+################################################################################
+#
+################################################################################
+
+# test pitch
+for i in {1..130..10}; do ffplay -i in.xm -af afreqshift=shift=-$i,afreqshift=shift=+66 -autoexit -t 3.58 -loop 2  -hide_banner -loglevel error && echo $i; done
+
+for i in {1..130..10}; do ffplay -i in.xm -af afreqshift=shift=+$i,afreqshift=shift=-66 -autoexit -t 3.58 -loop 2  -hide_banner -loglevel error && echo $i; done
+
+for i in {1..2500..200}; do ffplay -i in.xm -af afreqshift=shift=+$i,afreqshift=shift=-2400 -autoexit -t 1.88 -loop 2  -hide_banner -loglevel error && echo $i; done
+
+for i in {2350..2450..10}; do ffplay -i in.xm -af afreqshift=shift=+$i,afreqshift=shift=-2400,rubberband=440/432,rubberband=430/432,volume=volume=5dB -autoexit -t 1.88 -loop 2  -hide_banner -loglevel error && echo $i; done
+
+for i in {16..40..5}; do ffplay -i in.xm -af afreqshift=shift=+$i,afreqshift=shift=-30,rubberband=420/432,rubberband=460/432,volume=volume=7dB,crystalizer=i=3:c=0 -autoexit -t 17 -loop 2  -hide_banner -loglevel error && echo $i; done
+
+for i in {1..2..1}; do ffplay -i in.xm -af afreqshift=shift=-10,rubberband=420/432,rubberband=450/432,volume=volume=7dB,crystalizer=i=3:c=0 -autoexit -t 17 -loop 2  -hide_banner -loglevel error && echo $i; done
+
+
+
+# master sp
+for i in *.xm; do ffplay -i $i -af afreqshift=shift=-7,rubberband=420/432,rubberband=460/432,volume=volume=7dB,crystalizer=i=7:c=0 -autoexit -t 17  -hide_banner -loglevel error && echo $i; done
+
+for i in *.xm; do ffplay -i $i -af afreqshift=shift=+7,volume=volume=3dB,crystalizer=i=5:c=0,highpass=f=30,lowpass=f=6500,rubberband=420/432,rubberband=450/432 -autoexit -hide_banner -loglevel error && echo $i; done
+
+for i in *.xm; do ffplay -i $i -af afreqshift=shift=-q7,volume=volume=3dB,crystalizer=i=5:c=0,highpass=f=30,lowpass=f=6500,rubberband=420/432,rubberband=450/432 -autoexit -hide_banner -loglevel error && echo $i; done
+
+for i in *.wav; do ffplay -i $i -af afreqshift=shift=-49,afreqshift=shift=+42qq,volume=volume=3dB,crystalizer=i=5:c=0,highpass=f=30,lowpass=f=6500,rubberband=420/432,rubberband=450/432 -ss 00:00:00 -autoexit -hide_banner -loglevel error -ac 2 && echo $i; done
+
+
+# fx
+for i in *.xm; do ffplay -i $i -af afreqshift=shift=-577,rubberband=120/432,rubberband=1560/432,volume=volume=7dB,crystalizer=i=7:c=0 -autoexit -t 1.87 -loop 2  -hide_banner -loglevel error && echo $i; done
+
+for i in *.xm; do ffplay -i $i -af afreqshift=shift=-577,rubberband=120/432,rubberband=1560/432,volume=volume=3dB,crystalizer=i=7:c=0,highpass=f=300 -autoexit -t 1.87 -loop 2  -hide_banner -loglevel error && echo $i; done
+
+for i in *.wav; do ffplay -i $i -af afreqshift=shift=-20,volume=volume=3dB,crystalizer=i=5:c=0,highpass=f=30,lowpass=f=6500,rubberband=420/432,rubberband=450/432 -ss 00:00:14 -t 1.89 -loop 2 -autoexit -hide_banner -loglevel error && echo $i; done
+
+for i in *.wav; do ffplay -i $i -af afreqshift=shift=-20,volume=volume=3dB,crystalizer=i=5:c=0,highpass=f=30,lowpass=f=6500,rubberband=420/432,rubberband=450/432 -ss 00:00:14 -t 3.56 -loop 2 -autoexit -hide_banner -loglevel error && echo $i; done
+
+
+# super slow
+for i in *.wav; do ffplay -i $i -af afreqshift=shift=-7,volume=volume=3dB,crystalizer=i=5:c=0,highpass=f=30,lowpass=f=6500,rubberband=420/432,rubberband=450/432 -ss 00:00:14 -t 3.56 -loop 2 -autoexit -hide_banner -loglevel error -ac 1 && echo $i; done
+
+#detune
+for i in *.wav; do ffplay -i $i -af afreqshift=shift=-246,volume=volume=3dB,crystalizer=i=5:c=0,highpass=f=30,lowpass=f=6500,rubberband=420/432,rubberband=450/432 -ss 00:00:14 -t 3.56 -loop 2 -autoexit -hide_banner -loglevel error -ac 2 && echo $i; done
 
 
 
@@ -73,11 +117,13 @@ varseg=$(tr '\n' ',' < segments.txt) &&  ffmpeg -i mpxvm_loops.wav -f segment -s
 # test pitch
 ################################################################################
 
-RANDX=00:00:$(shuf -i10-59 -n1) && echo $RANDX && for i in {1..95..5}; do ffplay -hide_banner -loglevel error -i fumix_255_LMMSPianoEditCutMixFinal.wav -ss $RANDX  -t 1.78 -loop 2 -af 'aecho=0.2:0.98:8:0.99,afreqshift=shift=-'$i',volume=volume=3dB' -autoexit -ac 1 && echo $i && sleep 1; done
+RANDX=00:00:$(shuf -i10-59 -n1) && echo $RANDX && for i in {1..95..5}; do ffplay -hide_banner -loglevel error -i in.wav -ss $RANDX  -t 1.78 -loop 2 -af 'aecho=0.2:0.98:8:0.99,afreqshift=shift=-'$i',volume=volume=3dB' -autoexit -ac 1 && echo $i && sleep 1; done
 
 
 
-
+################################################################################
 # Audacity mix channels
+################################################################################
+
 # open original + set volume +10db / or +7db
 # clone original + make mono + invert + set +3db / or -3db
