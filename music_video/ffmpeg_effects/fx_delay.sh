c47e97c20644939aff34e1c83662e24fc3fe4c38
EM <maran.emil@gmail.com>
2023-11-02 19:00:38 +0100

    howto

--- music_video/ffmpeg_effects/fx_delay.sh
+++ music_video/ffmpeg_effects/fx_delay.sh
@@ -29,15 +29,15 @@ ffmpeg -i file.mkv -itsoffset 00:00:00.300 -i file.mkv -c:a copy -c:v copy -map 
 #https://superuser.com/questions/1719361/combining-two-audio-files-and-introducing-an-offset-with-ffmpeg
 #https://superuser.com/questions/1713053/mixing-audio-into-video-and-adjusting-volume
 
-#delay video by 3.84 seconds, use a command like this:
+# delay video by 3.84 seconds, use a command like this:
 ffmpeg -i "movie.mp4" -itsoffset 3.84 -i "movie.mp4" -map 1:v -map 0:a -c copy "movie-video-delayed.mp4"
 
-#delay audio by 3.84 seconds, use a command like this:
+# delay audio by 3.84 seconds, use a command like this:
 ffmpeg -i "movie.mp4" -itsoffset 3.84 -i "movie.mp4" -map 0:v -map 1:a -c copy "movie-audio-delayed.mp4"
 
-#ffmpeg -i 3.mp3 -af adelay=100000|100000 delayed.mp3
+# ffmpeg -i 3.mp3 -af adelay=100000|100000 delayed.mp3
 
-#2 tracks
+# 2 tracks
 ffmpeg -i p0.wav -i p1.wav \
   -filter_complex   "aevalsrc=0:d=2[s1];[s1][1:a]concat=n=2:v=0:a=1[ac2];[0:a]apad[ac1];[ac1][ac2]amerge=2[a]" -map "[a]"  \
   output.m4a
