3a45a810e7496f41b9658ae6d6d928106264e65c
mpxvm <maran.emil@gmail.com>
2026-09-07 18:46:36 +0200

    howto

--- music_video/ffmpeg_effects/fx_crop_jumping_effect.sh
+++ music_video/ffmpeg_effects/fx_crop_jumping_effect.sh
@@ -5,3 +5,22 @@ ffmpeg -i in1.mp4 -vf "crop=in_w/2:in_h/2:(in_w-out_w)/2+((in_w-out_w)/2)*sin(n/
 
 # add saturation
 ffmpeg -i output.mp4 -vf eq=saturation=1.5 -y output2.mp4
+
+
+
+# Cropping
+# sudo apt install flatpak
+# flatpak install flathub org.kde.kdenlive
+# 
+# flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+# sudo apt install gnome-software-plugin-flatpak
+# flatpak install flathub <app-id>
+# flatpak run <app-id>
+# flatpak update
+# flatpak uninstall <app-id>
+# 
+# sudo apt update
+# sudo apt install ffmpeg
+# ffmpeg -i input.mp4 -vf "crop=out_w:out_h:x:y" output.mp4
+# ffplay -i input.mp4 -vf "crop=570:440:619:463" 
+# ffmpeg -i input.mp4 -vf "crop=570:440:619:463" out.mp4
